b71aae3e45d3c85189fade84292512f1d7122f83
darshitkansara94 <48837243+darshitkansara94@users.noreply.github.com>
2024-10-12 11:03:02 +0530

    Assignment
    Assignment

---
+++ 12-Oct-2024/Assignment.sql
@@ -0,0 +1,28 @@
+-- Assignment
+
+Create DB (ECommerce)
+	Create table Registration (Admin id = 1 and SuperAdmin id = 2)
+		Insert SP
+		Update SP
+		Select SP
+	Validate Login
+		From Registration (Verify using SP)
+			if user is valid =  'Valid user + Username'
+			If user is invalid  = 'Invalid user + Username'
+	Create table for Product
+		Product Id
+		Product Name
+		Product Inserted date
+		Product Modified date
+		Product Deleted Date
+		Product Inserted by
+	Create ProductTypes
+		ProductType id
+		Product Id (Reference to Product table)
+		ProductType Name
+		Product Price
+		Product Quantity		
+		ProductType Inserted date
+		ProductType Modified date
+		ProductType Deleted Date
+		ProductType Inserted by
